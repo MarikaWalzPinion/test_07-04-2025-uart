87bf39d57a5de3dae56edca2a06db20c93f74daa
MarikaWalzPinion <m.walz@pinion.eu>
2025-04-11 09:58:46 +0200

    first attempts with USART1, later switched to USART2

---
+++ build/Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,8 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "test_07-04-2025-uart.map"
+  )
+endif()
